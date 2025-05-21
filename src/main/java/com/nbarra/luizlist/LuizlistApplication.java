db5a2374968ec400648a749ee7dc23d39dfb7884
felipew22 <Lf0349522@gmail.com>
2025-05-21 20:23:03 -0300

    Initial commit

---
+++ src/main/java/com/nbarra/luizlist/LuizlistApplication.java
@@ -0,0 +1,13 @@
+package com.nbarra.luizlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuizlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuizlistApplication.class, args);
+	}
+
+}
