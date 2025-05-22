8897b65bf4855484dcf4b7561eb037ffde5e0c62
felipew22 <Lf0349522@gmail.com>
2025-05-22 19:35:52 -0300

    Endpoint

--- src/main/java/com/nbarra/luizlist/controllers/GameController.java
+++ src/main/java/com/nbarra/luizlist/controllers/GameController.java
@@ -1,8 +1,11 @@
 package com.nbarra.luizlist.controllers;
+import com.nbarra.luizlist.entities.Game;
+import com.nbarra.luizlist.entities.dto.GameAllDTO;
 import com.nbarra.luizlist.entities.dto.GameMinDTO;
 import com.nbarra.luizlist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +17,18 @@ public class GameController {
 @Autowired
 private GameService gameService;
 
-@GetMapping
+    @GetMapping(value = "/{id}")
+    public GameAllDTO findById(@PathVariable Long id) {
+        GameAllDTO result = gameService.findById(id);
+        return result;
+
+    }
+
+    @GetMapping
     public List<GameMinDTO> findAll() {
         List<GameMinDTO> result = gameService.findAll();
         return result;
     }
 }
+
+
--- src/main/java/com/nbarra/luizlist/services/GameService.java
+++ src/main/java/com/nbarra/luizlist/services/GameService.java
@@ -2,10 +2,12 @@ package com.nbarra.luizlist.services;
 
 
 import com.nbarra.luizlist.entities.Game;
+import com.nbarra.luizlist.entities.dto.GameAllDTO;
 import com.nbarra.luizlist.entities.dto.GameMinDTO;
 import com.nbarra.luizlist.repositeries.GameReposetory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +17,12 @@ public class GameService {
 
     @Autowired
     private GameReposetory gameReposetory;
+
+    @Transactional(readOnly = true)
+    public GameAllDTO findById(Long id) {
+        Game result = gameReposetory.findById(id).get();
+        return new GameAllDTO(result);
+    }
 
     public List<GameMinDTO> findAll() {
         List<Game> result = gameReposetory.findAll();
